8d0faa5e68579ddbcfb2b794d7b9ce361e9a1bde
Noel Sadwin <klizzinator@gmail.com>
2026-03-29 01:23:58 -0400

    basic functionality complete

--- pokopia_inter.py
+++ pokopia_inter.py
@@ -53,6 +53,7 @@ def initialize_db():
             );
             ''')
         db.commit()
+        print("Wrote to database")
         db.close()
 
 
@@ -69,12 +70,12 @@ class Pokemon:
         if rs is None:
             db.close()
             return False
-        self.fill_in_blanks(rs)
+        self.fill_in_blanks_from_db(rs)
         db.close()
         return True
 
-    def fill_in_blanks(self, rs):
-        # rs is a sqlite3.Row returned by fetch one
+    def fill_in_blanks_from_db(self, rs):
+        # rs is a sqlite3.Row returned by fetchone
         for columnname in rs.keys():
             if columnname in self.__dict__ and not self.__dict__[columnname]:
                 self.__dict__[columnname] = rs[columnname]
@@ -89,6 +90,7 @@ class Pokemon:
         cursor = db.cursor()
         cursor.execute("INSERT OR IGNORE INTO pokemon (name, habitat, location, flavor, discovered, satisfaction, houseid) VALUES (:name, :habitat, :location, :flavor, :discovered, :satisfaction, :houseid);", self.__dict__)
         db.commit()
+        print("Wrote to database")
         db.close()
 
     def update_existing_pokemon(self):
@@ -96,6 +98,7 @@ class Pokemon:
         cursor = db.cursor()
         cursor.execute("UPDATE pokemon SET habitat=:habitat, location=:location, flavor=:flavor, discovered=:discovered, satisfaction=:satisfaction, houseid=:houseid WHERE name=:name;", self.__dict__)
         db.commit()
+        print("Wrote to database")
         db.close()
 
 
@@ -104,26 +107,71 @@ def find_best_matches(name):
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
     meaty_query = cursor.execute('''
-        SELECT COUNT(*) as 'rating', p.name, p.location
-        --     , h.type, h.id, (SELECT COUNT(*) FROM pokemon WHERE houseid = h.id) AS currct, h.maxcount
+        SELECT COUNT(*) as 'rating', p.name, p.location, p.satisfaction
+--          , h.type, h.id, (SELECT COUNT(*) FROM pokemon WHERE houseid = h.id) AS currct, h.maxcount
           FROM likes pl
          INNER JOIN pokemon p ON pl.pokemon = p.name
-        --  INNER JOIN house h ON p.houseid = h.id
+--        INNER JOIN house h ON p.houseid = h.id
          WHERE p.name != ?
            AND p.habitat = (SELECT habitat FROM pokemon WHERE name = ?)
-           AND pl.id    IN (SELECT id FROM likes WHERE pokemon = ?)
+           AND pl.name  IN (SELECT DISTINCT name FROM likes WHERE pokemon = ?)
          GROUP BY p.name ORDER BY rating DESC;
         ''', (name, name, name))  # is there a better way to add these arguments...
-    print("man i dont want to variablize this header row. Pokemon. its pikachu")
+    print("Your most compatible roommates:")
     for row in meaty_query.fetchall():
-        print("\t".join(row[:]))
+        print("\t".join(str(c) for c in row[:]))
     db.close()
 
-    
-def get_house_info():
-    # TODO: new tk window for house info intake
-    houseid = 0
+
+def get_house_if_exists():
+    global roomies_var_list, houseid
+
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    rs = cursor.execute("SELECT DISTINCT houseid FROM pokemon WHERE name IN (?,?,?);", tuple(rv.get() for rv in roomies_var_list)).fetchone()
+    if rs:
+        houseid = rs[0]
+        print("Found an existing house id %s with those roommates" % houseid)
+    else:
+        houseid = None
+    db.close()
     return houseid
+
+
+def insert_new_house(desc, maxoccupancy):
+    global location_var, houseid, ditto_var
+
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO house (location, type, ditto, maxoccupancy) VALUES (?,?,?,?);",
+                   (location_var.get(), desc, int(ditto_var.get()), maxoccupancy))
+    rs = cursor.execute("SELECT last_insert_rowid();").fetchone()
+    if rs:
+        houseid = rs[0]
+    db.commit()
+    print("Wrote to database")
+    db.close()
+    return houseid
+
+
+def get_house_info():
+    global root, type_var, size_var, location_var, houseid
+
+    get_house_if_exists()
+
+    if not houseid:
+        int_occupancy = 1
+        str_type = type_var.get()
+        str_size = size_var.get()
+        if str_type in ["Pink", "Orange", "Gray", "Yellow", "Minecraft"] or (str_size in ["House", "Office"] and str_type != "Poke Ball"):
+            int_occupancy = 4
+        elif str_size == "Cottage":
+            int_occupancy = 2
+
+        insert_new_house(str_type + " " + str_size, int_occupancy)
+
+    root.destroy()
+
 
 def add_attr(table, name, value):
     if value:
@@ -131,13 +179,38 @@ def add_attr(table, name, value):
         cursor = db.cursor()
         cursor.execute("INSERT INTO %s (pokemon, name) VALUES (?, ?)" % table, (name, value))
         db.commit()
+        print("Wrote to database")
         db.close()
 
 
+def find_balanced_location(skill):
+    if skill:
+        db = sqlite3.connect(DB_NAME)
+        cursor = db.cursor()
+        load_bearing_query = cursor.execute('''
+            SELECT COUNT(*) as 'count', p.location, s.name
+              FROM pokemon p
+             INNER JOIN skill s ON s.pokemon = p.name
+             WHERE s.name = ?
+               AND p.location != ''
+             GROUP BY p.location
+             ORDER BY count;
+            ''', (skill,))
+        print("Your %s skill might be needed in: " % skill)
+        for row in load_bearing_query:
+            print("\t".join(str(c) for c in row[:]))
+
+
+def get_existing(table):
+    db = sqlite3.connect(DB_NAME)
+    cursor = db.cursor()
+    existing_items = [x[0] for x in cursor.execute("SELECT DISTINCT name FROM %s;" % table).fetchall()]
+    db.close()
+    return existing_items
+
+
 def route_input(*args):
-    houseid = None
-    if house_var.get():
-        houseid = get_house_info()
+    global root, name_var, location_var, habitat_var, skills_var_list, likes_var_list, flavor_var, satisfaction_var, house_var, houseid, roomies_var_list
 
     pkmndict = {
         "name": name_var.get(),
@@ -150,52 +223,114 @@ def route_input(*args):
         }
 
     pkmn = Pokemon(pkmndict)
-    pkmn.write_to_db()
+    if not pkmn.exists():
+        if house_var.get() and not houseid:
+            house_info_window()
+        pkmn.houseid = houseid
+        pkmn.insert_new_pokemon()
+        for skill in skills_var_list:
+            add_attr("skill", pkmn.name, skill.get())
+        for likes in likes_var_list:
+            add_attr("likes", pkmn.name, likes.get())
+    else:
+        print("This Pokemon already exists in the database!")
+        if house_var.get() and not pkmn.houseid:
+            house_info_window()
+            pkmn.houseid = houseid
+        pkmn.update_existing_pokemon()
 
-    for skill in skills_var_list:  # this requires the main window code to be "global" in order to access this variable - that might be true of all tkinter code I've ever written, but still
-        add_attr("skill", pkmn.name, skill.get())
-    for likes in likes_var_list:
-        add_attr("likes", pkmn.name, likes.get())
+    if not pkmn.discovered:
+        for skill in skills_var_list:
+            find_balanced_location(skill.get())
+    else:
+        find_best_matches(pkmn.name)
 
-    find_best_matches(pkmn.name)
+    try:
+        for rv in roomies_var_list:
+            if rv.get():
+                tmp_pkmn = Pokemon({"name":rv.get()})
+                if not tmp_pkmn.exists():
+                    print("Autosuggesting a new Pokemon...")
+                    pokemon_window(auto_name=tmp_pkmn.name, auto_houseid=houseid, auto_location=location_var.get())
+    except NameError:
+        print("some lazy code just executed")
+
+    pokemon_window()
 
 
-def get_existing(table):
-    db = sqlite3.connect(DB_NAME)
-    cursor = db.cursor()
-    existing_items = [x[0] for x in cursor.execute("SELECT DISTINCT name FROM %s;" % table).fetchall()]
-    db.close()
-    return existing_items
+def house_info_window():
+    global root, type_var, size_var, roomies_var_list, ditto_var
     
+    print("A house, you say?")
+    root.destroy()
+    root = tk.Tk()
+    type_var = tk.StringVar()
+    size_var = tk.StringVar()
+    ditto_var = tk.BooleanVar(value=False)
+    roomies_var_list = [tk.StringVar(),tk.StringVar(),tk.StringVar()]
 
-if __name__=="__main__":
+    type_options = ["Leaf", "Sand", "Stone", "City", "Pink", "Orange", "Gray", "Yellow", "Poke Ball", "Minecraft"]
+    size_options = ["Den", "Hut", "Cottage", "House", "Office"]
+
+    # Row 1
+    tk.Label(root, text="House info").grid(row=0, column=0, sticky="w")
+    ttk.Combobox(root, textvariable=type_var, values=type_options, state="readonly").grid(row=0, column=1, sticky="ew")
+    ttk.Combobox(root, textvariable=size_var, values=size_options, state="readonly").grid(row=0, column=2, sticky="ew")
+
+    # Row 2
+    tk.Label(root, text="Roomies (?)").grid(row=1, column=0, sticky="w")
+    for i in range(len(roomies_var_list)):
+        tk.Entry(root, textvariable=roomies_var_list[i]).grid(row=1, column=1+i, sticky="ew")
+
+    # Row 3
+    tk.Label(root, text="And Ditto?").grid(row=2, column=0, sticky="w")
+    tk.Checkbutton(root, variable=ditto_var).grid(row=2, column=1, sticky="w")
+    tk.Button(root, text="Submit", command=get_house_info).grid(row=2, column=3, sticky="ew")
+
+    root.mainloop()
+
+
+def pokemon_window(auto_name='', auto_location='', auto_houseid=0):
+    global root, name_var, location_var, habitat_var, skills_var_list, likes_var_list, flavor_var, satisfaction_var, house_var, houseid
+
+    try:
+        root.destroy()
+        print("Enter another Pokemon!")
+    except NameError:
+        print("Welcome to the Pokopia Habitat Harmonizer! Output shows up here.")
+    except tk.TclError:
+        print("Enter another Pokemon (code did something weird edition)")
     root = tk.Tk()
     root.title("Pokopia Habitat Harmonizer")
+    
+    houseid = auto_houseid
 
     # Variables
-    name_var         = tk.StringVar()
-    location_var     = tk.StringVar()
+    name_var         = tk.StringVar(value=auto_name)
+    location_var     = tk.StringVar(value=auto_location)
     habitat_var      = tk.StringVar()
     skills_var_list  = [tk.StringVar(), tk.StringVar()]
     likes_var_list   = [tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar(), tk.StringVar()]
     flavor_var       = tk.StringVar()
     satisfaction_var = tk.StringVar()
-    house_var        = tk.BooleanVar(value=False)
+    house_var        = tk.BooleanVar(value=houseid!=0)
 
     # Dropdown contents
-    location_options     = ["Fuschia", "Vermillion", "Pewter", "Saffron", "Palette"]
+    location_options     = ["Fuchsia", "Vermillion", "Pewter", "Saffron", "Palette"]
     habitat_options      = ["Humid", "Dry", "Warm", "Cold", "Bright", "Dark"]
-    flavor_options       = ["Sweet","Sour","Bitter","Dry"]
-    satisfaction_options = ["Iffy","Average","Nice","Great","Awesome"]
+    flavor_options       = ["Sweet", "Sour", "Bitter", "Dry", "Spicy"]
+    satisfaction_options = ["Iffy", "Average", "Nice", "Great", "Awesome"]
 
     likes_suggestions    = get_existing("likes")
+    likes_suggestions.sort()
     skill_suggestions    = get_existing("skill")
+    skill_suggestions.sort()
 
-    # Row 0: Name | TEXTBOX | Discovered | CHECKBOX
+    # Row 0
     tk.Label(root, text="Name").grid(row=0, column=0, sticky="w")
     tk.Entry(root, textvariable=name_var).grid(row=0, column=1, sticky="ew")
 
-    # Row 1: Location | DROPDOWN | Habitat | DROPDOWN
+    # Row 1
     tk.Label(root, text="Location").grid(row=1, column=0, sticky="w")
     ttk.Combobox(root, textvariable=location_var,
                  values=location_options, state="readonly").grid(row=1, column=1, sticky="ew")
@@ -203,7 +338,7 @@ if __name__=="__main__":
     ttk.Combobox(root, textvariable=habitat_var,
                  values=habitat_options, state="readonly").grid(row=1, column=3, sticky="ew")
 
-    # Row 2: Skill 1 | DROPDOWN | Skill 2 | DROPDOWN
+    # Row 2
     tk.Label(root, text="Skill 1").grid(row=2, column=0, sticky="w")
     ttk.Combobox(root, textvariable=skills_var_list[0],
                  values=skill_suggestions, state="normal").grid(row=2, column=1, sticky="ew")
@@ -211,7 +346,7 @@ if __name__=="__main__":
     ttk.Combobox(root, textvariable=skills_var_list[1],
                  values=skill_suggestions, state="normal").grid(row=2, column=3, sticky="ew")
 
-    # Row 3: Likes | TEXTBOX | TEXTBOX | TEXTBOX
+    # Row 3
     tk.Label(root, text="Likes").grid(row=3, column=0, sticky="w")
     ttk.Combobox(root, textvariable=likes_var_list[0],
                  values=likes_suggestions, state="normal").grid(row=3, column=1, sticky="ew")
@@ -220,7 +355,7 @@ if __name__=="__main__":
     ttk.Combobox(root, textvariable=likes_var_list[2],
                  values=likes_suggestions, state="normal").grid(row=3, column=3, sticky="ew")
 
-    # Row 4: (empty) | TEXTBOX | TEXTBOX | DROPDOWN
+    # Row 4
     ttk.Combobox(root, textvariable=likes_var_list[3],
                  values=likes_suggestions, state="normal").grid(row=4, column=1, sticky="ew")
     ttk.Combobox(root, textvariable=likes_var_list[4],
@@ -228,15 +363,28 @@ if __name__=="__main__":
     ttk.Combobox(root, textvariable=flavor_var,
                  values=flavor_options, state="readonly").grid(row=4, column=3, sticky="ew")
 
-    # Row 5: Satisfaction | DROPDOWN | House | CHECKBOX
+    # Row 5
     tk.Label(root, text="Satisfaction").grid(row=5, column=0, sticky="w")
     ttk.Combobox(root, textvariable=satisfaction_var,
                  values=satisfaction_options, state="readonly").grid(row=5, column=1, sticky="ew")
     tk.Label(root, text="House").grid(row=5, column=2, sticky="w")
     tk.Checkbutton(root, variable=house_var).grid(row=5, column=3, sticky="w")
 
-    # Row 6: (empty) | (empty) | (empty) | BUTTON
+    # Row 6
     tk.Button(root, text="Submit", command=route_input).grid(row=6, column=3, sticky="ew")
 
     # Run
     root.mainloop()
+    
+
+if __name__=="__main__":
+    if False:
+        db = sqlite3.connect(DB_NAME)
+        cursor = db.cursor()
+        cursor.execute("DELETE FROM house WHERE id = 3;")
+        db.commit()
+        print("Wrote to database")
+        db.close()
+        print("got rid of them")
+    pokemon_window()
+    
